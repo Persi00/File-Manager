2c878939239ca262c1f1052196cf312df955ccf0
Persi00 <77538257+Persi00@users.noreply.github.com>
2021-03-09 14:29:16 +0100

    Add files via upload

--- FM.py
+++ FM.py
@@ -6,32 +6,36 @@ path = os.getcwd()
 error = ""
 
 while True:
+    os.system("cls")
     items = os.listdir(path)
-    i=2
+    i=3
     print("Your current directory: " + path)
-    print("0) " + c("exit", "magenta") + "\n1) " + c("..", "green"))
+    print("0) " + c("exit", "magenta") + "\n1) " + c(".", "green") + "\n2) " + c("..", "green"))
     for item in items:
         color = "white"
         if os.path.isdir(os.path.abspath(item)):
             color = "green"
         elif os.path.isfile(os.path.abspath(item)):
             color = "cyan"
-        else:
+        elif os.path.islink(os.path.abspath(item)):
             color = "yellow"
         print(str(i) + ") " + c(item, color))
         i+=1
     number = int(input(error + "Choose the number: "))
     if number == 0:
+        os.system("cls")
         exit()
     elif number == 1:
-        path = os.path.abspath(path + "\\..")
+        path = os.path.abspath(os.path.join(path + "."))
         error = ""
-    elif number > i-1 and number < 0:
-        error = c("incorrect number\n", "red")
+    elif number == 2:
+        path = os.path.abspath(os.path.join(path, ".."))
+        error = ""
+    elif (number >= i) or (number < 0):
+        error = c("incorrect number\n", "red", attrs=['bold'])
     else:
-        if os.path.isfile(os.path.abspath(items[number-2])):
-            os.system("start " + items[number-2])
+        if os.path.isfile(os.path.abspath(items[number-3])):
+            os.system("start " + items[number-3])
         else:
-            path = os.path.abspath(path + "\\" + items[number-2])
+            path = os.path.abspath(os.path.join(path, items[number-3]))
         error = ""
-    os.system("cls")
